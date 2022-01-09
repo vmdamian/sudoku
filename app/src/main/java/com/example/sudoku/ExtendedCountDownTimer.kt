3df8a9354685c6ed181e4c84a4b9f4c8fb38823d
Mihai Damian Visan <visanmihaidamian@gmail.com>
2022-01-09 19:30:02 +0200

    Actual game minimal implementation.
    Implemented default game logic. Integration with other fragments is still pending, some things are placeholders.
    
    Did some cleanup around some boilerplate code.

---
+++ app/src/main/java/com/example/sudoku/ExtendedCountDownTimer.kt
@@ -0,0 +1,40 @@
+package com.example.sudoku
+
+import android.os.CountDownTimer
+
+// ExtendedCountDownTimer is a wrapper over the CountDownTimer that allows to be paused and extended.
+// There is some loss of precision here (more or less equal to the tickInterval) as the internal countdown
+// is only updated at every tick, so don't use this if precision is key for you, or lower the tickInterval
+// as much as possible is that is affordable in your use case.
+// In the Sudoku countdown precision is not so relevant and we can afford dropping some (less than 1000ms anyway)
+class ExtendedCountDownTimer(
+    var durationMillis: Long,
+    var tickIntervalMillis: Long, var finishHandler: LoseHandler, var tickHandler : TickHandler) {
+
+    private lateinit var timer : CountDownTimer
+
+    fun start() {
+        timer = object : CountDownTimer(durationMillis, tickIntervalMillis) {
+            override fun onTick(remainingMillis: Long) {
+                durationMillis = remainingMillis
+                tickHandler(remainingMillis)
+            }
+
+            override fun onFinish() {
+                finishHandler()
+            }
+        }
+
+        timer.start()
+    }
+
+    fun extend(extensionMillis : Long) {
+        timer.cancel()
+        durationMillis += extensionMillis
+        timer.start()
+    }
+
+    fun pause() {
+        timer.cancel()
+    }
+}
