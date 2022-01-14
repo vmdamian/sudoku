ea4d48c032db3778f5edb0e7e81c1bc215aed4b2
Mihai Iacov <mihaimxd@gmail.com>
2022-01-14 13:01:19 +0200

    Add a mock scoreboard with a listing view (#5)
    - The scoreboard uses a RecyclerView for dynamically listing the entries in the scoreboard

--- app/src/main/java/com/example/sudoku/Scoreboard.kt
+++ app/src/main/java/com/example/sudoku/Scoreboard.kt
@@ -1,12 +1,16 @@
 package com.example.sudoku
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.sudoku.databinding.FragmentScoreboardBinding
 
 class Scoreboard : Fragment() {
@@ -21,7 +25,29 @@ class Scoreboard : Fragment() {
         binding.buttonBackScoreboard.setOnClickListener {
             findNavController().navigate(R.id.action_scoreboard_to_mainMenu)
         }
+        setupRecyclerView(binding)
 
         return binding.root
+    }
+
+    private fun setupRecyclerView(binding: FragmentScoreboardBinding) {
+        val recyclerView = binding.root.findViewById<RecyclerView>(R.id.recycler_view_scoreboard)
+        val layoutManager = binding.root.findViewById<ConstraintLayout>(R.id.layout_manager_recycler_scoreboard)
+        recyclerView.layoutManager = LinearLayoutManager(layoutManager.context)
+        val adapter = ScoreboardViewAdapter(getHighScoresDataset())
+        recyclerView.adapter = adapter
+    }
+
+    // TODO (future PR): replace mock data with an actual call to the DB collection.
+    private fun getHighScoresDataset(): List<ScoreboardEntryModel> {
+        return listOf(
+            ScoreboardEntryModel("#", "Player name", "Score"),
+            ScoreboardEntryModel("1", "Awesome Awesome-o", "100"),
+            ScoreboardEntryModel("2", "Bro Broski", "85"),
+            ScoreboardEntryModel("3", "CC", "10"),
+            ScoreboardEntryModel("4", "Davie", "5"),
+            ScoreboardEntryModel("13", "Nobody Near Nowhere Nonending Name", "0"),
+
+        )
     }
 }
